0a5ee571105d8ea10f9c7fa95524e598261b0521
Omel1311 <omel1311@gmail.com>
2022-07-25 21:39:03 +0300

    first clone lesson 3

---
+++ lesson3.py
@@ -0,0 +1,25 @@
+print("hello")
+print("fff", "ddd", sep="  ", end=" ")
+print("dcdc")
+
+
+my_best_scores = (10+2)
+print(my_best_scores)
+
+inter = input("enter a number: ")
+
+var = int(inter)
+
+print(inter)
+
+''' hkhkjhkjhkj '''
+
+""" ;k[pk pppppppppppppppppppp
+pppppppppppppppppppppppppppppppppp"""
+print(type(10))
+
+id(print(my_best_scores))
+
+
+
+print("dewdede", "fffff" )
